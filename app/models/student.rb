7db83aa9f237cc36003d48cbe6b7eeba59ef1ba3
Apprentice <apprentice@dbc07.local>
2013-09-12 10:46:40 -0700

    create students table

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,19 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+  validates :email, :format => { :with => /.+@.+\..{2,}/}
+  validates :email, :uniqueness => true
+  validates :age, :numericality => { :greater_than => 5 }
+  validates :phone, :format => { :with => /\d{3}.*\d{3}.*\d{4}/}
+
+  def name
+    self.first_name + " " + self.last_name
+  end
+
+  def age
+    now = Date.today
+    age = now.year - self.birthday.year - ((now.month > self.birthday.month || (now.month == self.birthday.month && now.day >= self.birthday.day)) ? 0 : 1)
+  end
 end
+
+
